a63fc7f88428fb281e1e249ad52ae79534bf8e5f
BORI <88658551+Kbomi16@users.noreply.github.com>
2022-08-09 22:08:51 +0900

    Add files via upload

---
+++ Chap9/main.js
@@ -0,0 +1,16 @@
+let myName = "HEY!!";
+let email = 'kimbomi2172@naver.com';
+let hello = `HEllo ${123}?!`;
+
+console.log(myName);
+console.log(email);
+console.log(hello);
+
+let user = {
+  name:'hi',
+  age: 85
+};
+
+console.log(user);
+console.log(user.age);
+console.log(user.name);
