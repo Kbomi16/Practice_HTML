a2332a6a39e71d080bca62258cb6922936650d89
BORI <88658551+Kbomi16@users.noreply.github.com>
2022-08-10 21:04:30 +0900

    Chap9

--- Chap9/main.js
+++ Chap9/main.js
@@ -14,3 +14,19 @@ let user = {
 console.log(user);
 console.log(user.age);
 console.log(user.name);
+
+let boxEl = document.querySelector('.box');
+
+console.log(boxEl);
+
+boxEl.addEventListener('click', function () {
+  console.log('click~');
+  boxEl.classList.add('active');
+  console.log(
+    boxEl.classList.contains('active')
+  );
+  boxEl.classList.remove('active');
+  console.log(
+    boxEl.classList.contains('active')
+  );
+});
---
+++ Chap9/main1.js
@@ -0,0 +1,6 @@
+const boxEl = document.querySelector('.box');
+
+console.log(boxEl.textContent);
+
+boxEl.textContent = 'hey';
+console.log(boxEl.textContent);
